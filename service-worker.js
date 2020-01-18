35bbb01864366abc5fe96347e15c3e4b43bfc855
Andrei Dragu <andrei.catalin7@gmail.com>
2020-01-18 22:34:50 +0000

    Sat Jan 18 22:34:50 UTC 2020

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mvm-web/precache-manifest.20fa9a69f16edc7cf74eddf53b3b2ecc.js"
+  "/mvm-web/precache-manifest.ec6ac75311fea9b9869c93eb3f59692a.js"
 );
 
 self.addEventListener('message', (event) => {
