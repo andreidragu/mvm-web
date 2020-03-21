f522ad712e878a40a09d0f420ab2bb03448ced1f
Andrei Dragu <andrei.catalin7@gmail.com>
2020-03-21 12:32:38 +0000

    Sat Mar 21 12:32:38 UTC 2020

---
+++ precache-manifest.cda163a5289a25aebf6d1489332b7703.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3726b983b189cd3861aa5845cf91d4d",
+    "url": "/mvm-web/index.html"
+  },
+  {
+    "revision": "0f7620ad50010fc56e2a",
+    "url": "/mvm-web/static/js/2.61364bb3.chunk.js"
+  },
+  {
+    "revision": "f5788910ea0df09bfcd690a8d696dec6",
+    "url": "/mvm-web/static/js/2.61364bb3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "467fd0aaf431a61a1d84",
+    "url": "/mvm-web/static/js/main.8bd61ffe.chunk.js"
+  },
+  {
+    "revision": "1a6b696752c85f9a3741",
+    "url": "/mvm-web/static/js/runtime-main.60ee3b0d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mvm-web/precache-manifest.6da4f9e00afcd523d69406c0c5764cc3.js"
+  "/mvm-web/precache-manifest.cda163a5289a25aebf6d1489332b7703.js"
 );
 
 self.addEventListener('message', (event) => {
